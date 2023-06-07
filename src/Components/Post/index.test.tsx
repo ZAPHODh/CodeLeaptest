217efe6f357a4b192ae88925fc42f76142b763ce
ZAPHODh <luispaulo.ni@gmail.com>
2023-06-06 23:41:57 -0300

    removing unecessary tests

--- src/Components/Post/index.test.tsx
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import { Post } from './';
-
-describe('<Post Component />', () => {
-  it('should render', () => {});
-});
